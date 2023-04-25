9b438991b2e7ae149fa3b96ba1d1c831cf07a581
Akshay Anand <86616444+Akshay2931@users.noreply.github.com>
2023-04-25 10:28:59 +0530

    HTTP details

--- Assignment2/pcap_analysis.py
+++ Assignment2/pcap_analysis.py
@@ -1,10 +1,39 @@
+#from scapy.layers.http import HTTP
 from scapy.all import *
+
+
+def http_plot(get_count,post_count):
+    pass
+
+def conn_plot(half_open_conn,closed_conn):
+    pass
+
+def protocol_plot(icmp_count,udp_count,tcp_count):
+    pass
+
+def http_display(http_list):
+    
+    for packet in http_list:
+
+        raw_data = packet[Raw].load
+        status_line = raw_data.split(b"\r\n")[0]
+        status_parts = status_line.split(b" ")
+        status_code = status_parts[1]
+        reason_phrase = b" ".join(status_parts[2:])
+
+        headers = b"\r\n".join(raw_data.split(b"\r\n\r\n")[0].split(b"\r\n")[1:])
+        print(f"HTTP/{status_line.split(b' ')[0].decode()} {status_code.decode()} {reason_phrase.decode()}")
+        print(headers.decode())
+        print("=" * 50)
+
 
 def pcap_analysis(filename):
     
     packets = rdpcap(filename)
 
     print("The total number of packets is {}.".format(len(packets)))
+
+    http_list = []
 
     conn = set()
     half_open_conn = set()
@@ -41,9 +70,11 @@ def pcap_analysis(filename):
                 half_open_conn.remove((src_ip,dest_ip,src_port,dest_port))
                 closed_conn.add((src_ip,dest_ip,src_port,dest_port))
 
-        if packet.haslayer(TCP) and packet.haslayer(Raw) and b'HTTP' in packet[Raw].load:
+        if packet.haslayer(TCP) and packet.haslayer(Raw) and b'HTTP/1.' in packet[Raw].load:
 
-            method = packet[Raw].load.split()[0].decode('utf-8')
+            http_list.append(packet)
+
+            method = packet[Raw].load.split(b" ")[0].decode('utf-8')
             if method == 'GET':
                 get_count+=1
             elif method == 'POST':
@@ -51,15 +82,19 @@ def pcap_analysis(filename):
             
 
 
+    print()
     print("The number of icmp connections are {} and udp connections are {} and tcp connections are {}.".format(icmp_count,udp_count,tcp_count))
     print("Number of closed connections are {} and half-open connections are {}".format(len(half_open_conn),len(closed_conn)))
     print("Total number of connections is {}".format(len(conn)))
     print("The number of get requests are {} and post requests are {}.".format(get_count,post_count))
+    print("The number of HTTP packet is {}.".format(len(http_list)))
+    print("="*50)
 
 
     protocol_plot(icmp_count,udp_count,tcp_count)
     conn_plot(len(half_open_conn),len(closed_conn))
     http_plot(get_count,post_count)
+    http_display(http_list)
 
 
 
