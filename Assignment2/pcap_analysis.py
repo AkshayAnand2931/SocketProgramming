4f48cf5b8425ed03d7a9644a176988bc7d6927a7
Akshay Anand <86616444+Akshay2931@users.noreply.github.com>
2023-04-25 12:46:08 +0530

    pie charts

--- Assignment2/pcap_analysis.py
+++ Assignment2/pcap_analysis.py
@@ -1,15 +1,32 @@
-#from scapy.layers.http import HTTP
 from scapy.all import *
+import matplotlib.pyplot as plt
 
 
 def http_plot(get_count,post_count):
-    pass
 
-def conn_plot(half_open_conn,closed_conn):
-    pass
+    label = ["Get Request","Post Request"]
+    data = [get_count,post_count]
+    plt.pie(data,labels=label,autopct='%1.1f%%')
+    plt.title("GET Vs POST requests")
+    plt.show()
+
+def conn_plot(open_conn,closed_conn,half_open):
+
+    label = ["Open Connections","Closed Connections","Half-Open Connections"]
+    data = [open_conn,closed_conn,half_open]
+    plt.pie(data,labels=label,autopct='%1.1f%%')
+    plt.title("Open connections vs closed connections")
+    plt.show()
+
 
 def protocol_plot(icmp_count,udp_count,tcp_count):
-    pass
+    label=['ICMP','UDP','TCP']
+    data=[icmp_count,udp_count,tcp_count]
+    plt.bar(label, data)
+    plt.title("Number of packets have different protocols")
+    plt.xlabel("Protocol")
+    plt.ylabel("Number of Packets")
+    plt.show()
 
 def http_display(http_list):
     
@@ -22,7 +39,7 @@ def http_display(http_list):
         reason_phrase = b" ".join(status_parts[2:])
 
         headers = b"\r\n".join(raw_data.split(b"\r\n\r\n")[0].split(b"\r\n")[1:])
-        print(f"HTTP/{status_line.split(b' ')[0].decode()} {status_code.decode()} {reason_phrase.decode()}")
+        print(f"{status_line.split(b' ')[0].decode()} {status_code.decode()} {reason_phrase.decode()}")
         print(headers.decode())
         print("=" * 50)
 
@@ -30,14 +47,15 @@ def http_display(http_list):
 def pcap_analysis(filename):
     
     packets = rdpcap(filename)
-
+    print()
     print("The total number of packets is {}.".format(len(packets)))
 
     http_list = []
 
     conn = set()
-    half_open_conn = set()
+    open_conn = set()
     closed_conn = set()
+    half_open = set()
 
     get_count = 0
     post_count = 0
@@ -62,12 +80,14 @@ def pcap_analysis(filename):
             dest_port = packet[TCP].dport
             flags = packet[TCP].flags
 
-            if flags & 0x02:
+            if flags & 0x02: #Check for SYN flag
                 conn.add((src_ip,dest_ip,src_port,dest_port))
-            elif flags & 0x10 and (src_ip,dest_ip,src_port,dest_port) in conn:
-                half_open_conn.add((src_ip,dest_ip,src_port,dest_port))
-            elif flags & 0x01 and (src_ip,dest_ip,src_port,dest_port) in half_open_conn:
-                half_open_conn.remove((src_ip,dest_ip,src_port,dest_port))
+                half_open.add((src_ip,dest_ip,src_port,dest_port))
+            if flags & 0x10 and (src_ip,dest_ip,src_port,dest_port) in half_open: #Check for ACK flag
+                open_conn.add((src_ip,dest_ip,src_port,dest_port))
+                half_open.remove((src_ip,dest_ip,src_port,dest_port))
+            if flags & 0x01 and (src_ip,dest_ip,src_port,dest_port) in open_conn: #Check for FIN flag
+                open_conn.remove((src_ip,dest_ip,src_port,dest_port))
                 closed_conn.add((src_ip,dest_ip,src_port,dest_port))
 
         if packet.haslayer(TCP) and packet.haslayer(Raw) and b'HTTP/1.' in packet[Raw].load:
@@ -84,7 +104,7 @@ def pcap_analysis(filename):
 
     print()
     print("The number of icmp connections are {} and udp connections are {} and tcp connections are {}.".format(icmp_count,udp_count,tcp_count))
-    print("Number of closed connections are {} and half-open connections are {}".format(len(half_open_conn),len(closed_conn)))
+    print("Number of closed connections are {} and open connections are {} and the half-open connections are {}.".format(len(closed_conn),len(open_conn),len(half_open)))
     print("Total number of connections is {}".format(len(conn)))
     print("The number of get requests are {} and post requests are {}.".format(get_count,post_count))
     print("The number of HTTP packet is {}.".format(len(http_list)))
@@ -92,7 +112,7 @@ def pcap_analysis(filename):
 
 
     protocol_plot(icmp_count,udp_count,tcp_count)
-    conn_plot(len(half_open_conn),len(closed_conn))
+    conn_plot(len(open_conn),len(closed_conn),len(half_open))
     http_plot(get_count,post_count)
     http_display(http_list)
 
